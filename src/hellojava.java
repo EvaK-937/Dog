fc3c701facf0f307db0ecb72f251e45704c5f2ad
Grisha <mestmestmest42@gmail.com>
2020-09-02 13:28:44 +0300

    create project

---
+++ src/hellojava.java
@@ -0,0 +1,10 @@
+package ru.mirea2419.pr1;
+import java.lang.System;
+public class hellojava
+
+{
+    public static void main (String[] args)
+    {
+     System.out.println("Hello Java");
+    }
+}
